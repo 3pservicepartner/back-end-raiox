2d46c97c3698afe9b46551655f64ee3679a730a3
3pservicepartner <3pservicepartner2@gmail.com>
2025-05-18 13:09:01 -0300

    commit 18052025-2

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const portApp = 8085
 //CONFIGURAÇÕES
 // cors
 const corsOptions ={
-    origin:['http://localhost:3000', 'http://56.125.31.77:3000'], 
+    origin:['http://localhost:3000', 'https://www.raiox3p.com'], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
